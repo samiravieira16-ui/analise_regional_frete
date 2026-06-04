7ee6f9dd961cdd5764393b64f51ccfbf6632fa3b
samiravieira16-ui <samiravieira16@gmail.com>
2026-06-04 13:01:44 -0300

    atualização 3

--- analise_regional.py
+++ analise_regional.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import seaborn as sns
-import sys
 import os
 
 # Ajuste automático do diretório de trabalho para compatibilidade com o VSCode Interactive Window
